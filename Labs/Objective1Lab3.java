aefb6f41c7c8617c91e13361ca4f93d9d7a25549
alfarog1 <alfaro.geovanny@gmail.com>
2019-02-27 14:29:36 -0700

    finish Obj1 lab3

---
+++ Labs/Objective1Lab3.java
@@ -0,0 +1,8 @@
+public class Objective1Lab3 {
+  public static void main(String[] args) {
+
+    System.out.println("Name : Geovanny Alfaro");
+    System.out.println("Age : 38");
+    System.out.println("State : Tegucigalpa");
+  }
+}
