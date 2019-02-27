49d55e77994cdda81f5bb1bf5f99f7bbd6051f80
alfarog1 <alfaro.geovanny@gmail.com>
2019-02-27 14:36:43 -0700

    finish jthw ex5

---
+++ JavaTheHardWay/CreatingVariables.java
@@ -0,0 +1,33 @@
+public class CreatingVariables {
+	public static void main( String[] args ) {
+		int x, y, z, age;
+		double seconds, e, checking, savings;
+		String firstName, lastName, title;
+		
+		x = 10;
+		y = 400;
+		z = 77;
+		age = 39;
+	
+		
+		seconds = 4.71;
+		e = 2.71828182845904523536;
+		checking = 1.89;
+		savings = 545.67;
+		
+		firstName = "Graham";
+		lastName = "Mitchell";
+		title = "Mr.";
+		
+		
+		System.out.println( "The variable x contains " + x);
+		System.out.println( "The value " + y + " is stored in the variable y." );
+		System.out.println( "The experiment took " + seconds + " seconds." );
+		System.out.println( "A favorite irrational # is Euler's number: " + e );
+		System.out.println( "Hopefully you have more than $" + checking + "!" );
+		System.out.println( "My name's " + title + " " + firstName + " " + lastName );
+		System.out.println( "How do I have " + savings + " " + "in my savings?" );
+		System.out.println( "We can resolve by adding " + x + " + " + y + " + " + z );
+		System.out.println();
+	}	
+}	
