308d540c0f263c2b0da7a035c7eb23093af30c55
alfarog1 <alfaro.geovanny@gmail.com>
2019-02-27 13:28:52 -0700

    finish jthw ex 1

---
+++ JavaTheHardWay/FirstProg.java
@@ -0,0 +1,6 @@
+public class FirstProg {
+	public static void main( String[] args ) {
+		System.out.println( "I, Geovanny, am determined to learn how to code.");
+		System.out.println( "Today's date is 23 Feb 2019" );
+	}
+}
